b713205ee4ef20a48d68c0c29ddac9746ddf41d2
Anderson Bairo <ander_slash@yahoo.com.br>
2021-10-11 23:04:56 -0300

    Closes #4: Creates and defines iot action models

---
+++ apps/iot_actions/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+import uuid
+
+
+
+class CaseIOTBase(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class IOTAction(CaseIOTBase):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200, blank=True)
+    topic = models.CharField(max_length=200)
+    active = models.BooleanField(default=False)
+
+
+class IOTEvent(CaseIOTBase):
+    SENT = 'SENT'
+    FAILED = 'FAILED'
+    UNDEFINED = 'UNDEFINED'
+    EVENT_STATUS_CHOICES = (
+        (SENT, 'Message sent'),
+        (FAILED, 'Failed'),
+        (UNDEFINED, 'Undefined')
+    )
+
+    iot_action = models.ForeignKey(IOTAction, on_delete=models.PROTECT)
+    status = models.CharField(choices=EVENT_STATUS_CHOICES, default=UNDEFINED,
+                              max_length=20)
+    value = models.SmallIntegerField()
+
