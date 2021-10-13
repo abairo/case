bf7f5ba7ed13616a17eb9a77cbb2a3d82c31d21f
Anderson Bairo <ander_slash@yahoo.com.br>
2021-10-12 22:08:35 -0300

    configures model classes to display in admin site

--- apps/iot_actions/admin.py
+++ apps/iot_actions/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import IOTAction, IOTEvent
 
-# Register your models here.
+
+@admin.register(IOTAction)
+class IOTActionAdmin(admin.ModelAdmin):
+    readonly_fields = ('id', 'created_at', 'updated_at',)
+
+
+@admin.register(IOTEvent)
+class IOTEventAdmin(admin.ModelAdmin):
+    readonly_fields = ('id', 'iot_action', 'status', 'value',
+                       'user', 'created_at', 'updated_at',)
